b9800c0a19dba79d635139da5d5a01d26eacfffd
Youssef-bj <68934844+Youssef-bj@users.noreply.github.com>
2020-08-29 14:04:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NzQ5MjIxNTA4MTM5NzEyNTUy.X0o05w.MFFEZafkyETsz-uFu4VXtFXlJsA',    
+    TOKEN: 'NzQ5MjIxNTA4MTM5NzEyNTUy.X0o05w.BAeVaemRxSrjaWiweEGy0pOJqDo',    
     YT_API_KEY: 'api ', 
     prefix: '#',
     devs: ['739267088228810893']
